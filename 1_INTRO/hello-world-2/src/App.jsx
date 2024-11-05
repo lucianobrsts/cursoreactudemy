2c6bb313022317f8508cf86ec580ec055b86432c
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-05 09:20:04 -0300

    First commit

---
+++ 1_INTRO/hello-world-2/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World 2</h1>
+      <h2>Criando, configurando e executando mudanças no projeto React</h2>
+    </>
+  )
+}
+
+export default App
