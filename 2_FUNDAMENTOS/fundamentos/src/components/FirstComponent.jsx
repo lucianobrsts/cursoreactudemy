59b8c45a408d19ebcc397bf54ed4eab5f990f935
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-05 10:48:49 -0300

    Adiciona comentários

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -1,7 +1,17 @@
+// arquivo de estilo
 
 const FirstComponent = () => {
+// essa função faz isso
+
+/*
+Comentário de 
+multiplas 
+linhas
+*/
+
     return (
         <div>
+            {/* Algum comentário */}
             <h2>Meu primeiro componente</h2>
             <p className="teste">Meu texto</p>
         </div>
