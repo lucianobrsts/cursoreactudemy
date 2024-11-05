4c15d2a01f0c278f24a878198f6d965848d5c75c
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-05 12:16:16 -0300

    Renderiza funções

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -6,6 +6,14 @@ const Events = () => {
         console.log("Você ativou o evento!")
     }
 
+    const renderSomething = (x) => {
+        if(x) {
+            return <h2>Renderizando isso!</h2>
+        } else {
+            return <h2>Também posso renderizar isso!</h2>
+        }
+    };
+
     return (
         <div>
             <div>
@@ -24,9 +32,11 @@ const Events = () => {
                     }
                 }}
                 >Clique aqui, por favor!
-                
                 </button>
             </div>
+
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     )
 }
