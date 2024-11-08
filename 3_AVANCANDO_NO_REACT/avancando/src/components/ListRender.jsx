7ef7eea119f4e5a5ca99f62fe71c93d6e8911fc3
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-07 22:05:54 -0300

    Entendendo a propriedade key

--- 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.jsx
+++ 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.jsx
@@ -3,11 +3,24 @@ import { useState } from 'react'
 const ListRender = () => {
     const [list] = useState(["Luciano", "Matheus", "Josias", "Pedro"])
 
+    const [users] =useState([
+        {id: 1, name: "Luciano", age: 49},
+        {id: 65345, name: "Matheus", age: 31},
+        {id: 4544544, name: "Josias", age: 28},
+        {id: 1414, name: "Pedro", age: 44}
+    ])
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>{user.name} - {user.age}</li>
             ))}
         </ul>
     </div>
