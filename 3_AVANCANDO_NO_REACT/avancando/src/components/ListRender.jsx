1bbcf80212910438b3df9b0053d4009a8e3a3b77
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-08 14:05:28 -0300

    Previous state

--- 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.jsx
+++ 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const ListRender = () => {
-    const [list] = useState(["Luciano", "Matheus", "Josias", "Pedro"])
+    const [list] = useState(["Luciano", "Matheus", "Josias", "Pedro"]);
 
-    const [users] =useState([
+    const [users, setUsers] =useState([
         {id: 1, name: "Luciano", age: 49},
-        {id: 65345, name: "Matheus", age: 31},
-        {id: 4544544, name: "Josias", age: 28},
-        {id: 1414, name: "Pedro", age: 44}
-    ])
+        {id: 2, name: "Matheus", age: 31},
+        {id: 3, name: "Josias", age: 28},
+        {id: 4, name: "Pedro", age: 44}
+    ]);
 
-  return (
+    const deleteRandam = () => {
+        const randamNumber = Math.floor(Math.random() * 5);
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers)
+            return prevUsers.filter((user) => randamNumber !== user.id);
+        });
+    };
+
+    return (
     <div>
+        { /*Render sem key primeiramente*/ }
         <ul>
             {list.map((item, i) => (
                 <li key={i}>{item}</li>
@@ -20,11 +30,14 @@ const ListRender = () => {
 
         <ul>
             {users.map((user) => (
-                <li key={user.id}>{user.name} - {user.age}</li>
+                <li key={user.id}>
+                    {user.name} - {user.age}
+                </li>
             ))}
         </ul>
+        <button onClick={deleteRandam}>Delete random user</button>
     </div>
-  )
+    );
 }
 
 export default ListRender
