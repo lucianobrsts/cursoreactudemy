9c8a3561caa12f75585446816167f99bb37c479d
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-09 15:31:33 -0300

    Css inline dinâmico

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Luciano Brito");
+
   return (
     <div className="root">
       {/* CSS Global */}
@@ -11,10 +16,27 @@ function App() {
       <MyComponent />
       <p>Este parágrafo é do app.jsx</p>
 
-      {/* CSS de Component */}
-      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid gold" }}>
+      {/* CSS inline */}
+      <p
+        style={{ color: "blue", padding: "25px", borderTop: "2px solid gold" }}
+      >
         Este elemento foi estilizado de forma inline
       </p>
+
+      {/* CSS inline dinamico */}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        Css dinâmico
+      </h2>
+
+      <h2
+        style={
+          name === "Luciano Brito"
+            ? { color: "green", backgroundColor: "#000" }
+            : null
+        }
+      >
+        Teste nome
+      </h2>
     </div>
   );
 }
