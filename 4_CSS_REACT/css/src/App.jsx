e7f485983e6aaacab8366e5e429c408548f9de6e
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-09 15:17:27 -0300

    Css inline

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -5,11 +5,16 @@ function App() {
   return (
     <div className="root">
       {/* CSS Global */}
-        <h1>CSS com React</h1>
+      <h1>CSS com React</h1>
 
       {/* CSS de Component */}
-        <MyComponent />
-        <p>Este parágrafo é do app.jsx</p>
+      <MyComponent />
+      <p>Este parágrafo é do app.jsx</p>
+
+      {/* CSS de Component */}
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid gold" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
     </div>
   );
 }
