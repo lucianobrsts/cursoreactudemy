4833ee236c0c44d992079d0bab32bad400d437c7
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-09 12:17:45 -0300

    Css de componente

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="root">
+      {/* CSS Global */}
         <h1>CSS com React</h1>
+
+      {/* CSS de Component */}
+        <MyComponent />
+        <p>Este parágrafo é do app.jsx</p>
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>CSS de component</h1>
+            <p>Este é o parágrafo do componente</p>
+            <p className="my-comp-paragrafo">Este componente também é do componente</p>
+        </div>
+    )
+}
+
+export default MyComponent
