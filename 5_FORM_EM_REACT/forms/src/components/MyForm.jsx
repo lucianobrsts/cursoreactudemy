f744fea3339c52daf9ffa98535c0a1f3ee4a63a0
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-09 21:11:47 -0300

    Gerenciamento de dados no input

--- 5_FORM_EM_REACT/forms/src/components/MyForm.jsx
+++ 5_FORM_EM_REACT/forms/src/components/MyForm.jsx
@@ -1,23 +1,38 @@
-import './MyForm.css'
+import { useState } from "react";
+import "./MyForm.css";
 
 const MyForm = () => {
+  // 3- Gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleNome = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(name);
   return (
     <div>
-        {/* 1 - criação de form */}
-        <form>
-            <div>
-                <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder='Digite o seu nome' />
-            </div>
-            {/* 2 - label envolvendo input */}
-            <label>
-                <span>E-mail:</span>
-                <input type="text" name='email' placeholder='Digite os eu email' />
-            </label>
-            <input type="submit" value="Enviar" />
-        </form>
+      {/* 1 - criação de form */}
+      <form>
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleNome}
+          />
+        </div>
+        {/* 2 - label envolvendo input */}
+        <label>
+          <span>E-mail:</span>
+          <input type="text" name="email" placeholder="Digite os eu email" />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
-  )
+  );
 };
 
-export default MyForm
+export default MyForm;
