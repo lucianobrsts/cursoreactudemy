2166448496184ebc7da67caca7c5458ccbe72ade
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-10 20:34:31 -0300

    Lógica pra fim do jogo

--- 6_PROJETO_SECRET_WORD/secretword/src/App.jsx
+++ 6_PROJETO_SECRET_WORD/secretword/src/App.jsx
@@ -14,6 +14,8 @@ const stages = [
   { id: 3, name: "end" },
 ];
 
+const guessesQty = 3;
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
@@ -22,7 +24,7 @@ function App() {
   const [letters, setLetters] = useState([]);
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
   const pickWordAndCategory = () => {
@@ -71,13 +73,27 @@ function App() {
         ...actualWrongLetters,
         normalizedLetter,
       ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
     }
   };
 
-  console.log(guessedLetters);
-  console.log(wrongLetters);
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+
+  useEffect(() => {
+    if(guesses <= 0) {
+      clearLetterStates();
+
+      setGameStage(stages[2].name)
+    }
+  }, [guesses]);
   
   const retryGame = () => {
+    setScore(0);
+    setGuesses(guessesQty);
     setGameStage(stages[0].name);
   };
 
