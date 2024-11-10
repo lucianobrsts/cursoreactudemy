a57a0e80bf58b7bfa5110107d895ec90aec054e0
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-10 09:10:15 -0300

    Criando componentes do projeto

---
+++ 6_PROJETO_SECRET_WORD/secretword/src/components/Game.jsx
@@ -0,0 +1,11 @@
+import './Game.css'
+
+const Game = () => {
+    return (
+        <div>
+            <h2>Game</h2>
+        </div>
+    )
+}
+
+export default Game
---
+++ 6_PROJETO_SECRET_WORD/secretword/src/components/GameOver.jsx
@@ -0,0 +1,11 @@
+import './GameOver.css'
+
+const GameOver = () => {
+    return (
+        <div>
+            <h2>GameOver</h2>
+        </div>
+    )
+}
+
+export default GameOver
