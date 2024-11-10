ef781a019f14ff922dfb8a29d640d5942d45d848
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-09 23:37:38 -0300

    Criando o projeto secret word

---
+++ 6_PROJETO_SECRET_WORD/secretword/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Banco", "Volante", "Buzina", "Calota", "Amortercedor"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacate", "Melância", "Ata", "Tangerina"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Coração", "Fígado", "Dedos", "Unhas"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Mémoria", "Fonte"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "Java", "Python", "Lógica"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Bolo", "Cuscuz", "Queijo", "Manteiga"]
+};
