6b5bfc5fc6efff17df0ccca0ee9cdc05fc0e6b54
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-12 07:43:52 -0300

    Cursom hook para resgate de dados

--- 7_REQ_HHTP_REACT/httpreact/src/App.jsx
+++ 7_REQ_HHTP_REACT/httpreact/src/App.jsx
@@ -2,22 +2,29 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
+// 4- Custom hook
+import { useFetch } from "./hooks/useFetch";
+
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+
+  // 4- custom hook
+  const { data: items } = useFetch(url);
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
   // 1- Resgatando dados
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url);
-      const data = await res.json();
-      setProducts(data);
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     setProducts(data);
+  //   }
+  //   fetchData();
+  // }, []);
 
   // 2- add de produtos
   const handleSubmit = async (e) => {
@@ -31,14 +38,14 @@ function App() {
     const res = await fetch(url, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(product)
+      body: JSON.stringify(product),
     });
 
     // 3- Carregamento dinâmico
     const addedProduct = await res.json();
-    setProducts((prevProducts) => [...prevProducts, addedProduct])
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
 
     setName("");
     setPrice("");
@@ -48,7 +55,7 @@ function App() {
     <div>
       <h1>Lista de Produtos</h1>
       <ul>
-        {products.map((product) => (
+        {items && items.map((product) => (
           <li key={product.id}>
             {product.name} - R$: {product.price}
           </li>
