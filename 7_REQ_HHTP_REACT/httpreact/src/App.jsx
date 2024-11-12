60b7d5fcffcd806b250e126b847649583b67bac5
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-11 22:17:32 -0300

    Carregamento dinâmico de dados

--- 7_REQ_HHTP_REACT/httpreact/src/App.jsx
+++ 7_REQ_HHTP_REACT/httpreact/src/App.jsx
@@ -6,8 +6,10 @@ const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-  //Resgatando dados
+  // 1- Resgatando dados
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url);
@@ -17,14 +19,65 @@ function App() {
     fetchData();
   }, []);
 
+  // 2- add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+
+    // 3- Carregamento dinâmico
+    const addedProduct = await res.json();
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    setName("");
+    setPrice("");
+  };
+
   return (
     <div>
       <h1>Lista de Produtos</h1>
       <ul>
         {products.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
         ))}
       </ul>
+      <div className="add_product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
