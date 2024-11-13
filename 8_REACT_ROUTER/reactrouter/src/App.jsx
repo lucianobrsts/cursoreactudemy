1809c8c4568f8cc49897d146acc55dfe6282be6b
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-13 10:08:18 -0300

    Configurando o react router

--- 8_REACT_ROUTER/reactrouter/src/App.jsx
+++ 8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
 import './App.css'
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
