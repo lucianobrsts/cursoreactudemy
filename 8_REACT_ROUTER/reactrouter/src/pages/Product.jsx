840d31b9dc8fdfc3faf1870936d2cb7d5260782e
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-14 09:57:37 -0300

    Carregamento dinâmico de dados

--- 8_REACT_ROUTER/reactrouter/src/pages/Product.jsx
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.jsx
@@ -8,13 +8,21 @@ const Product = () => {
   const { id } = useParams();
 
   // 5- Carregamento dado individual
-  const url = "http://localhost:3000/products" + id;
+  const url = "http://localhost:3000/products/" + id;
   const { data: product, loading, error } = useFetch(url);
   console.log(product);
 
   return (
     <>
-      <p>ID do produto: {id}</p>
+      {/*<p>ID do produto: {id}</p>*/}
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R$: {product.price}</p>
+        </div>
+      )}
     </>
   );
 };
