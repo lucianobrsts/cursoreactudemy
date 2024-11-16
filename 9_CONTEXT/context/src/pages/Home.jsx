8af6b04a666b9a868f8aa541194c418a315c0376
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-16 09:47:48 -0300

    Refatorando contexto com hooks

--- 9_CONTEXT/context/src/pages/Home.jsx
+++ 9_CONTEXT/context/src/pages/Home.jsx
@@ -1,16 +1,21 @@
-import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
+// import { useContext } from "react";
+// import { CounterContext } from "../context/CounterContext";
 import ChangeCounter from "../components/ChangeCounter";
 
+// 4- Refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const Home = () => {
-  const { counter } = useContext(CounterContext);
+  // const { counter } = useContext(CounterContext);
+
+  const {counter} = useCounterContext();
 
   return (
     <div>
       <h2>Home</h2>
       <p>Valor do contador: {counter}</p>
 
-      {/* Alterando valor do contexto */}
+      {/* 3- Alterando valor do contexto */}
       <ChangeCounter />
     </div>
   );
