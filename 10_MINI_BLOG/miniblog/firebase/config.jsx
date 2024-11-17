6050e2a1c0c1ef4ab86518bf57ca74914077dc1a
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-16 22:08:54 -0300

    Integra firebase ao react

---
+++ 10_MINI_BLOG/miniblog/firebase/config.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKrW8V9DEGpn7UzUdtNieZekHHtfOuD8c",
+  authDomain: "miniblog-1dcfa.firebaseapp.com",
+  projectId: "miniblog-1dcfa",
+  storageBucket: "miniblog-1dcfa.firebasestorage.app",
+  messagingSenderId: "116532581488",
+  appId: "1:116532581488:web:b6383924e18979289090b5",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+export { db };
