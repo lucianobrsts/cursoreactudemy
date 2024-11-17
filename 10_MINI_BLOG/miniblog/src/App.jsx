33f68b01b525008ae89fe1e9efe6e98f596d6a55
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-16 23:30:19 -0300

    Adiciona css para navbar e footer

--- 10_MINI_BLOG/miniblog/src/App.jsx
+++ 10_MINI_BLOG/miniblog/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -14,12 +16,14 @@ function App() {
           v7_startTransition: true,
         }}
       >
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ 10_MINI_BLOG/miniblog/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import styles from "./Navbar.module.css";
+
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
+        Mini <span>Blog</span>
+      </NavLink>
+
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
