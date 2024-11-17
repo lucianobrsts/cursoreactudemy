0656bc97916c9726d046455a1576d7785dde5cf0
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-16 22:28:42 -0300

    Configura o react router

--- 10_MINI_BLOG/miniblog/src/App.jsx
+++ 10_MINI_BLOG/miniblog/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
-
   return (
     <div>
-      <h2>Miniblog</h2>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
