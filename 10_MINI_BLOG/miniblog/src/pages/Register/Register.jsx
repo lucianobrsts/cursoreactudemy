5183086c95f9ee6e5090756fdcd651bf62c997eb
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-18 09:27:53 -0300

    registra usuário no sistema

--- 10_MINI_BLOG/miniblog/src/pages/Register/Register.jsx
+++ 10_MINI_BLOG/miniblog/src/pages/Register/Register.jsx
@@ -2,6 +2,8 @@ import styles from "./Register.module.css";
 
 import { useState, useEffect } from "react";
 
+import { useAuthentication } from "../../hooks/useAuthentication";
+
 export default function Register() {
   const [displayName, setDisplayName] = useState("");
   const [email, setEmail] = useState("");
@@ -9,7 +11,9 @@ export default function Register() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     setError("");
@@ -25,8 +29,16 @@ export default function Register() {
       return;
     }
 
-    console.log(user);
+    const res = await createUser(user);
+
+    console.log(res);
   };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
 
   return (
     <div className={styles.register}>
@@ -81,7 +93,12 @@ export default function Register() {
           />
         </label>
 
-        <button className="btn">Cadatrar</button>
+        {!loading && <button className="btn">Cadatrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
 
         {error && <p className="error">{error}</p>}
       </form>
