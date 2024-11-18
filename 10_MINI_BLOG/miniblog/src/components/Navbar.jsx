5a0b5adbb674737fbeffa2d3c577e4a5c917360d
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-17 22:51:54 -0300

    Cria o formulário de cadastro

--- 10_MINI_BLOG/miniblog/src/components/Navbar.jsx
+++ 10_MINI_BLOG/miniblog/src/components/Navbar.jsx
@@ -21,6 +21,24 @@ export default function Navbar() {
 
         <li>
           <NavLink
+            to="/login"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Entrar
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/register"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Cadastrar
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
             to="/about"
             className={({ isActive }) => (isActive ? styles.active : '')}
           >
