ea168d8877e175bbbd24253ed61dfa83eabcfe3a
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-19 21:56:03 -0300

    Ajusta mensagem de erro login

--- 10_MINI_BLOG/miniblog/src/hooks/useAuthentication.jsx
+++ 10_MINI_BLOG/miniblog/src/hooks/useAuthentication.jsx
@@ -70,6 +70,34 @@ export const useAuthentication = () => {
     signOut(auth);
   };
 
+  //login - sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta";
+      } else if (error.message.includes("invalid-credential")) {
+        systemErrorMessage = "Login ou senha inválidos, tente novamente";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -80,5 +108,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };
--- 10_MINI_BLOG/miniblog/src/pages/Login/Login.jsx
+++ 10_MINI_BLOG/miniblog/src/pages/Login/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const { createUser, error: authError, loading } = useAuthentication();
+  const { login, error: authError, loading } = useAuthentication();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,7 +21,7 @@ export default function Login() {
       password,
     };
 
-    const res = await createUser(user);
+    const res = await login(user);
 
     console.log(res);
   };
