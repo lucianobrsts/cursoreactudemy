517a66e1495f0aef4a7560b357aa9a12b44a1b01
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-26 23:13:05 -0300

    Ajuste no search

--- 10_MINI_BLOG/miniblog/src/pages/Search/Search.jsx
+++ 10_MINI_BLOG/miniblog/src/pages/Search/Search.jsx
@@ -1,16 +1,31 @@
 import styles from "./Search.module.css";
 
+import { Link } from "react-router-dom";
+
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
+import PostDetail from "../../components/PostDetail";
 
 export default function Search() {
   const query = useQuery();
   const search = query.get("q");
+  const { documents: posts } = useFetchDocuments("posts", search);
 
   return (
     <div>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        {posts && posts.lenght === 0 && (
+            <>
+                <p>Não foram encontrados posts a partir da sua busca...</p>
+                <Link to="/" className="btn btn-dark">
+                    voltar
+                </Link>
+            </>
+        )}
+
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 }
