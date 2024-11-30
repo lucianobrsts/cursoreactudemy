678ae673799e666d7ef5bd34e4d88e08f3a64a20
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-29 21:26:16 -0300

    Exibindo resultados da busca

--- 10_MINI_BLOG/miniblog/src/pages/Search/Search.jsx
+++ 10_MINI_BLOG/miniblog/src/pages/Search/Search.jsx
@@ -12,16 +12,16 @@ export default function Search() {
   const { documents: posts } = useFetchDocuments("posts", search);
 
   return (
-    <div>
+    <div className={styles.search_container}>
       <h2>Search</h2>
       <div>
-        {posts && posts.lenght === 0 && (
-            <>
+        {posts && posts.length === 0 && (
+            <div className={styles.noposts}>
                 <p>Não foram encontrados posts a partir da sua busca...</p>
                 <Link to="/" className="btn btn-dark">
                     voltar
                 </Link>
-            </>
+            </div>
         )}
 
         {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
