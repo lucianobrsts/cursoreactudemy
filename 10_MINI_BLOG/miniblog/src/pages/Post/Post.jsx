7342673a9071c1788d464b97ab9e7b198fac1a5e
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-30 23:35:45 -0300

    Ajuste no css da page post

--- 10_MINI_BLOG/miniblog/src/pages/Post/Post.jsx
+++ 10_MINI_BLOG/miniblog/src/pages/Post/Post.jsx
@@ -14,7 +14,7 @@ export default function Post() {
         <>
           <h1>{post.title}</h1>
           <img src={post.image} alt={post.title} />
-          <p>{post.body}</p>
+          <p className={styles.post_container_p}>{post.body}</p>
           <h3>Este post trata sobre: </h3>
 
           <div className={styles.tags}>
