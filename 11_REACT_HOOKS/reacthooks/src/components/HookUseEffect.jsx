3cce9b1b8d8e1c79b839620f89d927aa6ca40937
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-12-04 23:47:12 -0300

    Utilizando o array de dependências

--- 11_REACT_HOOKS/reacthooks/src/components/HookUseEffect.jsx
+++ 11_REACT_HOOKS/reacthooks/src/components/HookUseEffect.jsx
@@ -17,12 +17,25 @@ export const HookUseEffect = () => {
     console.log("Serei executado apenas uma vez!");
   }, []);
 
+  // 3- Item no array de dependencias
+  const [anotherNumber, setAnotherNumber] = useState(0);
+
+  useEffect(() => {
+    if (anotherNumber > 0) {
+      console.log("Sou executado apenas quando o anotherNumber muda!");
+    }
+  }, [anotherNumber]);
+
   return (
     <div>
       <h2>HookUseEffect</h2>
       <p>Number: {number}</p>
       <button onClick={changeSomething}>Executar!</button>
 
+      <p>AnotherNumber: {anotherNumber}</p>
+      <button onClick={() => setAnotherNumber(anotherNumber + 1)}>
+        Mudar anotherNumber
+      </button>
       <hr />
     </div>
   );
