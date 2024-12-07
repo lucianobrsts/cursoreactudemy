757ad093baa011bb39bb0a45ffb53ed8164ebad3
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-12-07 16:08:44 -0300

    useLayoutEffect

--- 11_REACT_HOOKS/reacthooks/src/components/HookUseLayoutEffect.jsx
+++ 11_REACT_HOOKS/reacthooks/src/components/HookUseLayoutEffect.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useLayoutEffect, useEffect, useState } from "react";
 
 const HookUseLayoutEffect = () => {
+  const [name, setName] = useState("Algum nome");
+
+  useEffect(() => {
+    console.log("2");
+    setName("Mudou de novo!");
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log("1");
+    setName("Outro nome!");
+  });
+
+  console.log(name);
+
   return (
     <div>
-        <h2>useLayoutEffect</h2>
+      <h2>useLayoutEffect</h2>
+      <p>Nome: {name}</p>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default HookUseLayoutEffect
+export default HookUseLayoutEffect;
--- 11_REACT_HOOKS/reacthooks/src/pages/Home/Home.jsx
+++ 11_REACT_HOOKS/reacthooks/src/pages/Home/Home.jsx
@@ -1,16 +1,18 @@
 import styles from "./Home.module.css";
 import React from "react";
 
-import HookUseState from "../../components/HookUseState";
-import HookUseReducer from "../../components/HookUseReducer";
-import { HookUseEffect } from "../../components/HookUseEffect";
-
 // useContext
 import { useContext } from "react";
 import { SomeContext } from "../../components/HookUseContext";
+
+//Hooks
+import { HookUseEffect } from "../../components/HookUseEffect";
+import HookUseState from "../../components/HookUseState";
+import HookUseReducer from "../../components/HookUseReducer";
 import HookUseRef from "../../components/HookUseRef";
 import HookUseCallback from "../../components/HookUseCallback";
 import HookUseMemo from "../../components/HookUseMemo";
+import HookUseLayoutEffect from "../../components/HookUseLayoutEffect";
 
 const Home = () => {
   const {contextValue} = useContext(SomeContext)
@@ -27,6 +29,7 @@ const Home = () => {
       <HookUseRef />
       <HookUseCallback />
       <HookUseMemo />
+      <HookUseLayoutEffect />
     </div>
   );
 };
