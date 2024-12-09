8c1cb37568ebc30e96b9f6f9fdff9b3f22c66c3a
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-12-09 07:17:34 -0300

    Rota de teste da api

--- 12_REACTGRAM/backend/app.js
+++ 12_REACTGRAM/backend/app.js
@@ -8,9 +8,14 @@ const port = process.env.PORT;
 
 const app = express();
 
-//config JSON and form data response
+// Config JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// routes
+const router = require("./routes/Router.js");
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`App rodando na porta ${port}`);
