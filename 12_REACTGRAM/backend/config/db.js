a037a262b39d016f220557b5e0d83ffb42566c37
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-12-10 00:37:35 -0300

    Conexão com o banco de dados

--- 12_REACTGRAM/backend/config/db.js
+++ 12_REACTGRAM/backend/config/db.js
@@ -1,3 +1,21 @@
 const mongoose = require("mongoose")
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
 
-// connection
+const conn = async () => {
+    try {
+        const dbCom = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.rvup2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+        )
+
+        console.log("Conectou com o bando!")
+
+        return dbCom
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+conn()
+
+module.exports = conn;
