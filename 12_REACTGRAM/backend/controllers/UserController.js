ceba744d0fc1b22912c13009770fbfbb69026644
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-12-20 23:58:34 -0300

    Ajustes

--- 12_REACTGRAM/backend/controllers/UserController.js
+++ 12_REACTGRAM/backend/controllers/UserController.js
@@ -50,8 +50,27 @@ const register = async (req, res) => {
 };
 
 // Sign user in
-const login = (req, res) => {
-  res.send("Login");
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  // Check if user exists
+  if (!User) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+
+  // Check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+  }
+
+  // Return user with token
+  res.status(201).json({
+    _id: newUser._id,
+    profileImage: user.profileImage,
+    token: generateToken(newUser._id),
+  });
 };
 
 module.exports = {
