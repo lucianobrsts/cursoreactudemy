c96d77f350aee4e58c73bed04fd1c0f6b984ca42
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-12-29 00:48:23 -0300

    Middleware de atualização de usuário

--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -2,19 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 // Controller
-const { register, login, getCurrentUser } = require("../controllers/UserController");
+const {
+  register,
+  login,
+  getCurrentUser,
+  update,
+} = require("../controllers/UserController");
 
 // Middlewares
 const validate = require("../middlewares/handleValidation");
 const {
   userCreateValidation,
   loginValidation,
+  userUpdateValidation,
 } = require("../middlewares/UseValidations");
-const authGuard = require("../middlewares/authGuard")
+const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/ImageUpload");
 
 // Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
-router.get("/profile", authGuard, getCurrentUser)
+router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
 
 module.exports = router;
